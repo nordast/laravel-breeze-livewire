008102dd8384344d53f449ddc9d07dcb86bbbb2f
nordast <nordast@gmail.com>
2025-01-04 15:13:16 +0200

    task form

--- app/Enums/StatusType.php
+++ app/Enums/StatusType.php
@@ -4,7 +4,17 @@ namespace App\Enums;
 
 enum StatusType: string
 {
-    public const STARTED = 'started';
-    public const IN_PROGRESS = 'in_progress';
-    public const DONE = 'done';
+    case STARTED = 'started';
+    case IN_PROGRESS = 'in_progress';
+    case DONE = 'done';
+
+
+    public function color(): string
+    {
+        return match ($this) {
+            self::STARTED => 'border-blue-500',
+            self::IN_PROGRESS => 'border-yellow-500',
+            self::DONE => 'border-green-500',
+        };
+    }
 }
