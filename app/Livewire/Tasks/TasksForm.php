9d50d33300f11eca4ef09a760e994cd94a301adf
nordast <nordast@gmail.com>
2025-01-05 13:19:06 +0200

    tasks manager

---
+++ app/Livewire/Tasks/TasksForm.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire\Tasks;
+
+use App\Livewire\Forms\TaskForm;
+use Livewire\Component;
+
+class TasksForm extends Component
+{
+    public TaskForm $form;
+
+    public function save()
+    {
+        $this->validate();
+        $this->form->create();
+        $this->form->reset();
+    }
+
+    public function render()
+    {
+        return view('livewire.tasks.tasks-form');
+    }
+}
--- app/Livewire/Tasks/TasksIndex.php
+++ app/Livewire/Tasks/TasksIndex.php
@@ -2,21 +2,10 @@
 
 namespace App\Livewire\Tasks;
 
-use App\Livewire\Forms\TaskForm;
-use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class TasksIndex extends Component
 {
-    public TaskForm $form;
-
-    public function save()
-    {
-        $this->validate();
-        $this->form->create();
-        $this->form->reset();
-    }
-
     public function render()
     {
         return view('livewire.tasks.tasks-index', [
