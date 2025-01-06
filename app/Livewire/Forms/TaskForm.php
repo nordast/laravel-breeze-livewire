72c322dac171dfc88846fd054db3d8cc905369f7
nordast <nordast@gmail.com>
2025-01-06 12:44:07 +0200

    update task

--- app/Livewire/Forms/TaskForm.php
+++ app/Livewire/Forms/TaskForm.php
@@ -2,31 +2,49 @@
 
 namespace App\Livewire\Forms;
 
+use App\Models\Task;
 use Livewire\Attributes\Rule;
 use Livewire\Form;
 
 class TaskForm extends Form
 {
-    #[Rule('required|min:3|max:50')]
+    public ?Task $task;
+    public $editMode = false;
+    #[Rule('required|min:5')]
     public $title;
-
-    #[Rule('required|min:3|max:50')]
+    #[Rule('required|min:5')]
     public $slug;
-
-    #[Rule('required|min:10|max:255')]
+    #[Rule('required|min:5')]
     public $description;
-
     #[Rule('required')]
     public $status;
-
     #[Rule('required')]
     public $priority;
-
     #[Rule('required')]
     public $deadline;
 
-    public function create() {
-        auth()->user()->tasks()->create($this->all());
-        request()->session()->flash('success', 'Task created successfully');
+    public function setTask(Task $task)
+    {
+        $this->task = $task;
+        $this->editMode = true;
+        $this->title = $task->title;
+        $this->slug = $task->slug;
+        $this->description = $task->description;
+        $this->status = $task->status;
+        $this->deadline = $task->deadline->format('Y-m-d');
+        $this->priority = $task->priority;
+    }
+
+    public function createTask()
+    {
+        if ($this->editMode) {
+            $this->task->update($this->all());
+            $this->reset();
+            request()->session()->flash('success', __('Task updated successfully'));
+        } else {
+            auth()->user()->tasks()->create($this->all());
+            $this->reset();
+            request()->session()->flash('success', __('Task created successfully'));
+        }
     }
 }
--- app/Livewire/Tasks/TasksForm.php
+++ app/Livewire/Tasks/TasksForm.php
@@ -3,6 +3,8 @@
 namespace App\Livewire\Tasks;
 
 use App\Livewire\Forms\TaskForm;
+use App\Models\Task;
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class TasksForm extends Component
@@ -12,11 +14,21 @@ class TasksForm extends Component
     public function save()
     {
         $this->validate();
-        $this->form->create();
+        $this->form->createTask();
         $this->dispatch('task-created');
-        $this->form->reset();
     }
 
+    #[On('edit-task')]
+    public function editTask($id)
+    {
+        $task = Task::findOrFail($id);
+        $this->form->setTask($task);
+    }
+
+    public function refresh()
+    {
+        $this->form->reset();
+    }
     public function render()
     {
         return view('livewire.tasks.tasks-form');
